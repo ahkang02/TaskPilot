12041575bbccc00c4d10cdeb47d6363bc301cd89
Chong Zhi Hong <zhihong.chong@bertelsmann.de>
2024-05-16 15:21:45 +0800

    Adding Back Files To Avoid Error From Building

---
+++ TaskPilot.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskPilot.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
