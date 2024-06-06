46e71586fe7559af99f482d3c1aa81e35ba2476c
Chong Zhi Hong <zhihong.chong@bertelsmann.de>
2024-06-06 11:43:10 +0800

    #83 Preparing Unit Test For Model Validation

---
+++ TaskPilot.Tests/AllowedExtensionTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using System.ComponentModel.DataAnnotations;
+using TaskPilot.Application.Common.Utility.CustomValidator;
+
+namespace TaskPilot.Tests
+{
+    [TestFixture]
+    public class AllowedExtensionsAttributeTests
+    {
+        [TestCase("test.jpg")]
+        [TestCase("test.png")]
+        [TestCase("test.jpeg")]
+        public void IsValid_ValidExtension_ReturnsSuccess(string fileName)
+        {
+            // Arrange
+            var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
+            var attribute = new AllowedExtensionsAttribute(allowedExtensions);
+
+            var mockFormFile = new Mock<IFormFile>();
+            mockFormFile.Setup(f => f.FileName).Returns(fileName);
+
+            // Act
+            var result = attribute.IsValid(mockFormFile.Object);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestCase("test.pdf")]
+        [TestCase("test.csv")]
+        [TestCase("test.doc")]
+        public void IsValid_InValidExtension_ReturnsError(string fileName)
+        {
+            // Arrange
+            var allowedExtensions = new string[] { ".jpg", ".png", ".jpeg" };
+            var attribute = new AllowedExtensionsAttribute(allowedExtensions);
+
+            var mockFormFile = new Mock<IFormFile>();
+            mockFormFile.Setup(f => f.FileName).Returns(fileName);
+
+            // Act
+            var result = attribute.IsValid(mockFormFile.Object);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+
+    }
+}
---
+++ TaskPilot.Tests/DateLessThanTodayTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskPilot.Application.Common.Utility.CustomValidator;
+
+namespace TaskPilot.Tests
+{
+    [TestFixture]
+    public class DateLessThanTodayTest
+    {
+        [TestCase("12/06/2024", ExpectedResult = true)]
+        [TestCase("05/06/2024", ExpectedResult = false)]
+        [TestCase("06/06/2024", ExpectedResult = true)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity(DateTime date)
+        {
+            DateLessThanToday dateLessThanToday = new DateLessThanToday();
+            return dateLessThanToday.IsValid(date.Date);
+        }
+    }
+}
