eeb32ad6b397595a80290eebb9ee1571767ae05f
Chong Zhi Hong <zhihong.chong@bertelsmann.de>
2024-06-19 14:22:01 +0800

    #119 Modifying the Chart API to has capture user, to filter the chart based on user
    Refine task data to user-specific in charts
    
    Updated ChartController.cs to filter chart data by the currently logged-in user, enhancing the relevance of the displayed task data. Changes include:
    - Added retrieval of the current user's username from their identity.
    - Implemented fetching of user details from the database using the username.
    - Modified task fetching logic to include only tasks assigned to the current user, using a new condition in the GetAllInclude method calls.
    - Removed previous methods that fetched tasks without user-specific filtering.

--- TaskPilot.Web/APIs/ChartController.cs
+++ TaskPilot.Web/APIs/ChartController.cs
@@ -19,11 +19,13 @@ namespace TaskPilot.Web.APIs
 
         public IEnumerable<ChartDTO> GetDueTasks()
         {
+            var username = User.Identity!.Name;
+            var currentUser = _unitOfWork.Users.Get(u => u.UserName == username);
 
             // Getting All The Task & Group Them Based On Respective Date
-            var dueTasks = _unitOfWork.Tasks.GetAll().GroupBy(x => x.DueDate).Select(x => new { Date = x.Key, DueTasksCount = x.Count() }).ToList();
-            var createdTask = _unitOfWork.Tasks.GetAll().GroupBy(x => x.Created.Date).Select(x => new { Date = x.Key, CreatedTasksCount = x.Count() }).ToList();
-            var resolvedTask = _unitOfWork.Tasks.GetAllInclude(null, "Status").Where(x => x.Status!.Description == "Closed").GroupBy(x => x.Updated!.Value.Date).Select(x => new { Date = x.Key, ResolvedTasksCount = x.Count() }).ToList();
+            var dueTasks = _unitOfWork.Tasks.GetAllInclude(u => u.AssignToId == currentUser.Id, null).GroupBy(x => x.DueDate).Select(x => new { Date = x.Key, DueTasksCount = x.Count() }).ToList();
+            var createdTask = _unitOfWork.Tasks.GetAllInclude(u => u.AssignToId == currentUser.Id, null).GroupBy(x => x.Created.Date).Select(x => new { Date = x.Key, CreatedTasksCount = x.Count() }).ToList();
+            var resolvedTask = _unitOfWork.Tasks.GetAllInclude(u => u.AssignToId == currentUser.Id, "Status").Where(x => x.Status!.Description == "Closed").GroupBy(x => x.Updated!.Value.Date).Select(x => new { Date = x.Key, ResolvedTasksCount = x.Count() }).ToList();
 
             // Combine All The Dates, Remove Duplicates
             var combinedDates = dueTasks.Select(x => x.Date.GetValueOrDefault()).Union(createdTask.Select(x => x.Date)).Union(resolvedTask.Select(x => x.Date)).Distinct().ToList();
