d0e98e4bf5236154a1e12735191da447c8a7ebb8
Chong Zhi Hong <zhihong.chong@bertelsmann.de>
2024-07-02 11:27:28 +0800

    #191 Added Profile Controller Unit Test [Increasing Code Coverage]

--- TaskPilot.Tests/ProfileControllerTest.cs
+++ TaskPilot.Tests/ProfileControllerTest.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.EntityFrameworkCore;
 using MockQueryable.Moq;
 using Moq;
+using NUglify.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -123,7 +124,8 @@ namespace TaskPilot.Tests
         public async Task EditDetail_InvalidModelState_ReturnViews()
         {
             // Arrange
-            var viewModel = new EditProfileViewModel {
+            var viewModel = new EditProfileViewModel
+            {
                 FirstName = "Test",
                 LastName = "User",
                 Email = "testuser@example.com",
@@ -200,6 +202,285 @@ namespace TaskPilot.Tests
             Assert.IsNotNull(result);
             Assert.That(result.TempData["SuccessMsg"], Is.EqualTo(Message.PROF_DETAIL_EDIT));
         }
+
+        [Test]
+        public async Task ChangeEmail_NullValue()
+        {
+            // Arrange
+
+
+            // Act
+            var result = await _controller.ChangeEmail(null, null, null) as RedirectToActionResult;
+
+            // Assert
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
+        [Test]
+        public async Task ChangeEmail_UserNotFound()
+        {
+            // Arrange
+            _mockUserManager.Setup(x => x.FindByIdAsync("1")).ReturnsAsync((ApplicationUser)null);
+
+            // Act
+            var result = await _controller.ChangeEmail("1", "test@example.com", "dummyToken") as RedirectToActionResult;
+
+            // Assert
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
+        [Test]
+        public async Task ChangeEmail_TokenExpired()
+        {
+            // Arrange
+            _mockUserManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(_mockUser);
+            _mockUserManager.Setup(x => x.ChangeEmailAsync(_mockUser, It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed());
+
+            // Act
+            var result = await _controller.ChangeEmail("1", "test@gmail.com", "dummyToken") as RedirectToActionResult;
+
+            // Assert
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
+        [Test]
+        public async Task ChangeEmail_Valid()
+        {
+            // Arrange
+            _mockUserManager.Setup(x => x.FindByIdAsync(It.IsAny<string>())).ReturnsAsync(_mockUser);
+            _mockUserManager.Setup(x => x.ChangeEmailAsync(_mockUser, It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+            // Act
+            var result = await _controller.ChangeEmail("1", "test@gmail.com", "dummyToken") as RedirectToActionResult;
+
+            // Assert
+            Assert.That(result, Is.TypeOf<RedirectToActionResult>());
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
+        [Test]
+        public async Task CallEditPassword_ReturnEditPasswordView()
+        {
+            // Arrange
+
+            // Act
+            var result = await _controller.EditPassword() as ViewResult;
+            var viewModel = result?.Model as EditProfilePasswordViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task EditPassword_InvalidModelStateReturnView()
+        {
+            // Arrange
+            var viewModel = new EditProfilePasswordViewModel
+            {
+                Id = "1",
+                CurrentPassword = "Test",
+                NewPassword = "Testing1234!!",
+                ConfirmPassword = "Testing1234!!"
+            };
+
+            _controller.ModelState.AddModelError("CurrentPassword", "Required");
+
+            // Act
+            var result = await _controller.EditPassword(viewModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ViewName, Is.EqualTo("EditPassword"));
+            Assert.That(result.Model, Is.EqualTo(viewModel));
+        }
+
+        [Test]
+        public async Task EditPassword_ValidModelState_PasswordMismatch_ReturnView()
+        {
+            // Arrange
+            var viewModel = new EditProfilePasswordViewModel
+            {
+                Id = "1",
+                CurrentPassword = "Test",
+                NewPassword = "Testing1234!!",
+                ConfirmPassword = "Testing1234!!"
+            };
+
+            _mockUserManager.Setup(x => x.FindByIdAsync(viewModel.Id)).ReturnsAsync(_mockUser);
+            _mockUserManager.Setup(x => x.ChangePasswordAsync(_mockUser, viewModel.CurrentPassword, viewModel.NewPassword)).ReturnsAsync(IdentityResult.Failed());
+
+            // Act
+            var result = await _controller.EditPassword(viewModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ViewName, Is.EqualTo("EditPassword"));
+            Assert.That(result.Model, Is.EqualTo(viewModel));
+            Assert.That(result.TempData["ErrorMsg"], Is.EqualTo(Message.PROF_PASS_EDIT_FAIL));
+        }
+
+        [Test]
+        public async Task EditPassword_ValidModelState_Valid()
+        {
+            // Arrange
+            var viewModel = new EditProfilePasswordViewModel
+            {
+                Id = "1",
+                CurrentPassword = "Test",
+                NewPassword = "Testing1234!!",
+                ConfirmPassword = "Testing1234!!"
+            };
+
+            _mockUserManager.Setup(x => x.FindByIdAsync(viewModel.Id)).ReturnsAsync(_mockUser);
+            _mockUserManager.Setup(x => x.ChangePasswordAsync(_mockUser, viewModel.CurrentPassword, viewModel.NewPassword)).ReturnsAsync(IdentityResult.Success);
+            _mockUserManager.Setup(x => x.UpdateAsync(_mockUser)).ReturnsAsync(IdentityResult.Success);
+
+            // Act
+            var result = await _controller.EditPassword(viewModel) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
+        [Test]
+        public async Task CallEditContact_ReturnEditContactView()
+        {
+            // Arrange
+
+            // Act
+            var result = await _controller.EditContact() as ViewResult;
+            var viewModel = result?.Model as EditContactViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task EditContact_InvalidModelState()
+        {
+            // Arrange
+            var viewModel = new EditContactViewModel
+            {
+                PhoneNumber = "601234567890",
+                UserPermissions = _mockPermission
+            };
+
+            _controller.ModelState.AddModelError("PhoneNumber", "Required");
+
+            // Act
+            var result = await _controller.EditContact(viewModel) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.TempData["ErrorMsg"], Is.EqualTo(Message.COMMON_ERROR));
+        }
+
+        [Test]
+        public async Task EditContact_ValidModelState()
+        {
+            // Arrange
+            var viewModel = new EditContactViewModel
+            {
+                PhoneNumber = "601234567890",
+                UserPermissions = _mockPermission
+            };
+
+            _mockUserManager.Setup(x => x.GenerateChangePhoneNumberTokenAsync(_mockUser, viewModel.PhoneNumber)).ReturnsAsync("dummyToken");
+            _mockSmsSender.Setup(x => x.SendSmsAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.EditContact(viewModel) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("VerifyPhoneNumber"));
+            Assert.That(result.RouteValues!["phoneNumber"], Is.EqualTo(viewModel.PhoneNumber));
+        }
+
+        [Test]
+        public void CallVerifyPhoneNumber_ReturnVerifyPhoneNumberView()
+        {
+            // Arrange
+            var phoneNumber = "601234567890";
+
+            // Act
+            var result = _controller.VerifyPhoneNumber(phoneNumber) as ViewResult;
+            var viewModel = result?.Model as VerifyPhoneNumberViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(viewModel!.PhoneNumber, Is.EqualTo(phoneNumber));
+        }
+
+        [Test]
+        public void CallVerifyPhoneNumber_ReturnError()
+        {
+            // Arrange 
+
+            // Act
+            var result = _controller.VerifyPhoneNumber(phoneNumber: null) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ViewName, Is.EqualTo("Error"));
+        }
+
+        [Test]
+        public async Task VerifyPhoneNumberInvalidModelState_ReturnView()
+        {
+            // Arrange
+            var viewModel = new VerifyPhoneNumberViewModel
+            {
+                PhoneNumber = "601234567890",
+                Code = "123456"
+            };
+
+            _controller.ModelState.AddModelError("Code", "Code is required");
+
+
+            // Act
+            var result = await _controller.VerifyPhoneNumber(viewModel) as ViewResult;
+            
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.Model, Is.EqualTo(viewModel));
+            Assert.That(result.TempData["ErrorMsg"], Is.EqualTo(Message.PROF_CONTACT_EDIT_FAIL));
+        }
+
+        [Test]
+        public async Task VerifyPhoneNumberValidModel_ReturnView()
+        {
+            // Arrange
+            var viewModel = new VerifyPhoneNumberViewModel
+            {
+                PhoneNumber = "601234567890",
+                Code = "123456"
+            };
+
+            _mockUserManager.Setup(x => x.ChangePhoneNumberAsync(_mockUser, viewModel.PhoneNumber, viewModel.Code)).ReturnsAsync(IdentityResult.Success);
+
+            // Act
+            var result = await _controller.VerifyPhoneNumber(viewModel) as RedirectToActionResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.ActionName, Is.EqualTo("Index"));
+            Assert.That(result.ControllerName, Is.EqualTo("Profile"));
+        }
+
     }
 
 }
